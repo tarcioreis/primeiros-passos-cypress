6009b9684daa51650d0e5344f11eece78c1f7fbf
tarcioreis <souzareistarcio@gmail.com>
2025-09-04 20:40:27 -0300

    Adding selectors in objects

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,19 +1,27 @@
 describe('template spec', () => {
+
+  let selectorsList = {
+      userNameField: '[name="username"]',
+      passwordField: '[name="password"]',
+      loginButton: '[type="submit"]',
+      topBarTitle: '.oxd-topbar-header-breadcrumb > .oxd-text'
+  };
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin');
-    cy.get('[name="password"]').type('admin123');
-    cy.get("[type='submit']").click();
+    cy.get(selectorsList.userNameField).type('Admin');
+    cy.get(selectorsList.passwordField).type('admin123');
+    cy.get(selectorsList.loginButton).click();
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard');
-  })
+    cy.get(selectorsList.topBarTitle).contains('Dashboard');
+  });
 
   it('Login - fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    cy.get('[name="username"]').type('Test');
-    cy.get('[name="password"]').type('Test');
-    cy.get('.oxd-button').click();
+    cy.get(selectorsList.userNameField).type('Test');
+    cy.get(selectorsList.passwordField).type('Test');
+    cy.get(selectorsList.loginButton).click();
     cy.get('.oxd-alert');
-  })
+  });
 
-})
+});
